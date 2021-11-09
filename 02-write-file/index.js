38f9a5da0877d222798b7ca57c05db5e3cef0203
Roman <sholomitskyR@yandex.by>
2021-11-09 03:48:22 +0300

    feat: fix problem with update files in task 6

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 
+
 const readline = require('readline');
 const { stdin: input, stdout: output } = require('process');
 
@@ -9,13 +10,14 @@ const { stdin: input, stdout: output } = require('process');
 const rl = readline.createInterface({ input, output });
 const way = path.resolve('02-write-file', 'text.txt')
 
+fs.open (way, 'w', (err) => 
+        {if (err) 
+            {throw err}})
 
 rl.question('Enter your text:', (answer) => {
     if (answer.includes('exit')) { 
       exit()} else {
-        fs.open (way, 'w', (err) => 
-        {if (err) 
-            {throw err}})
+        
         fs.appendFile(way, answer +'\n', function(){})
     }
 });
--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,70 +1,96 @@
 const fs = require('fs');
 const path = require('path');
-
-function deleteDir() {
-fs.rm("06-build-page/project-dist", {force: true, recursive: true}, (err) => {
-    if (err) {
-        console.log('The folder cant remove')
-    }})
-}
-deleteDir()
-
-
-
-fs.mkdir("06-build-page/project-dist", { recursive: true }, (err) => {
-    if (err) throw err;
-  });
 const mas = ['header', 'articles', 'about', 'footer']
 let data = '';
 
- 
 
 
-const stream = fs.createReadStream('06-build-page/template.html', 'utf-8');
 
 
-stream.on('data', chunk => data += chunk);
-stream.on('end', () => 
-mas.forEach(element => {
-    fs.readFile(`06-build-page/components/${element}.html`, 'utf-8', (err, part) => {
-        if (err) throw err;
-        data = data.replace(`{{${element}}}`, part)
-        fs.writeFile(
-            "06-build-page/project-dist/index.html",
-            data,
-            "utf-8",
-            (err) => {
-              if (err) throw err;
-            }
-          );
-      });    
+
+fs.readdir("06-build-page/project-dist", (err, items) => {
+    if (!err) {
+        updateDir()
+    } else {
+        createDir()
+        copyAssets()
+        mergeStyle()
+        fixHtml()
+    }
 })
-);
-stream.on('error', error => console.log('Error', error.message));
+
+
+async function updateDir() {
+    await fs.rm("06-build-page/project-dist", {force: true, recursive: true}, (err) => {
+        if (err) {
+            console.log('Не удалось обновить папку')
+        } else {
+            createDir()
+            copyAssets()
+            mergeStyle()
+            fixHtml()
+        }
+    })
+}
+
+
+
+function createDir(){
+    fs.mkdir("06-build-page/project-dist", { recursive: true }, (err) => {
+        if (err) throw err;
+    });
+}
+
+
+async function writed(element) {
+    const output = fs.createWriteStream("06-build-page/project-dist/index.html");
+    output.write(element)
+   
+}
+
+
+
+ function fixHtml() {
+    const stream = fs.createReadStream('06-build-page/template.html', 'utf-8');
+    stream.on('data', chunk => data += chunk);
+    stream.on('error', error => console.log('Error', error.message));
+     stream.on('end', () => 
+      mas.forEach(element => {
+            
+            fs.readFile(`06-build-page/components/${element}.html`, 'utf-8', (err, part) => {
+               
+                data = data.replace(`{{${element}}}`,  part)
+                writed(data)
+            });   
+            
+            
+        })
+     )}
+
 
 
 
 //merge styles
+function mergeStyle(){
+    fs.readdir(path.resolve(__dirname, 'styles'), {withFileTypes: true}, (err,files)=> {
+        if (err) {throw err}
+    files.forEach(file => {
+        if (file.isFile() && path.extname(file.name) === '.css') {
+            
 
-fs.readdir(path.resolve(__dirname, 'styles'), {withFileTypes: true}, (err,files)=> {
-    if (err) {throw err}
-   files.forEach(file => {
-       if (file.isFile() && path.extname(file.name) === '.css') {
+            const stream = new fs.ReadStream(`06-build-page/styles/${file.name}`, 'utf-8');
+    stream.on('readable', () => {
+    const data = stream.read();
+    if (data) {
+        fs.promises.appendFile(path.join(__dirname, 'project-dist/style.css'), data);
+    }
+    })
         
+        }
+    })
 
-        const stream = new fs.ReadStream(`06-build-page/styles/${file.name}`, 'utf-8');
-stream.on('readable', () => {
-  const data = stream.read();
-  if (data) {
-    fs.promises.appendFile(path.join(__dirname, 'project-dist/style.css'), data);
-  }
-})
-    
-       }
-   })
-
-})
-
+    })
+}
 
 //copy assets
 
@@ -76,7 +102,7 @@ pathCopy = path.join(__dirname, 'project-dist/assets')
 async function copyAssets() {
 
     await fs.mkdir(pathCopy, (err) => {
-        if (err) deleteDir();
+        if (err) console.log();
     });
 
 
@@ -85,11 +111,13 @@ async function copyAssets() {
             fs.mkdir(path.join(pathCopy, file), {recursive: true}, (err) => {
                 if (err) throw err;
             });
+
         const assets = path.join(pathSource, file);
         const assetsCopy = path.join(pathCopy, file);
 
             fs.readdir(assets, (err, items) => {
             for (const item of items) {
+                
                 fs.copyFile(path.join(assets, item), path.join(assetsCopy, item), fs.constants.COPYFILE_EXCL, (err) => {
                 if (err) {
                             process.exit();
@@ -100,7 +128,7 @@ async function copyAssets() {
     })
 }
 
-copyAssets()
+
   
 
 
